82198736bdc52819c974090b23053bf918744283
fermibot <ashwinuoh@gmail.com>
2023-10-18 17:24:53 -0700

    initial commit

---
+++ kafka-topics-test.sh
@@ -0,0 +1,5 @@
+#kafka_2.13-3.6.0/bin/kafka-server-start.sh kafka_2.13-3.6.0/config/kraft/server.properties &
+#
+#sh kafka_2.13-3.6.0/bin/kafka-topics.sh --bootstrap-server localhost://174.61.228.230:9092 --topic first_topic --create --partitions 3 --replication-factor 1
+
+
